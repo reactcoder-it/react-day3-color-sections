e7a765749c7f654b7b2109b1a0f2db972edb0b38
Vadim Pashaev <vmp@live.ru>
2018-10-09 00:09:01 +0400

    Updates

---
+++ precache-manifest.88781a2b4d990f54b17668aeec74169f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5af193cf38b51019f2ba",
+    "url": "/react-day3-color-sections/static/js/runtime~main.5af193cf.js"
+  },
+  {
+    "revision": "6b43816dd1cc9c37e8bd",
+    "url": "/react-day3-color-sections/static/js/main.6b43816d.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/react-day3-color-sections/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "6b43816dd1cc9c37e8bd",
+    "url": "/react-day3-color-sections/static/css/main.dd08e8da.chunk.css"
+  },
+  {
+    "revision": "774a439466345d312b5bdf1efa8f8431",
+    "url": "/react-day3-color-sections/index.html"
+  }
+];
